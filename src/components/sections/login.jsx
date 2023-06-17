6b0ed12b7de449e06842225f17277630525f9bae
Just2sire <le05onze2004@gmail.com>
2023-06-17 09:56:36 +0000

    Mise de place de l'authentification avec le backend Node

--- src/components/sections/login.jsx
+++ src/components/sections/login.jsx
@@ -1,38 +1,46 @@
-import { useContext, useRef, useState } from 'react';
+import axios from 'axios';
+import { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { NotifContext } from '../myHook/notif';
+import { toast } from 'react-toastify';
 const Login = () => {
     const email = useRef();
     const password = useRef();
     const [isLoading, setIsLoading] = useState(false);
     const [success, setSuccess] = useState(null);
-    const [error, setError] = useState(null);
     const navigate = useNavigate();
-    const { showNotif } = useContext(NotifContext);
 
     const authToken = localStorage.getItem('token');
-    console.log("AuthToken: ",  authToken);
 
     const handleLogout = () => {
         localStorage.removeItem('token');
-        navigate('/');
+        localStorage.removeItem('userId');
+        toast.success("Vous vous etes déconnecté", {
+            autoClose: 1000
+        })
+        setTimeout(() => {
+            navigate(-1);
+        }, 1000);
     };
 
-    const HandleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         setIsLoading(true);
+        const id = toast.loading("En cours de connexion", {
+            theme: 'light',
+            pauseOnHover: false
+        })
         const data = {
             email: email.current.value,
             password: password.current.value,
         };
 
-        fetch('http://localhost:1000/auth/login', 
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)
+        /*fetch('http://localhost:1000/auth/login',
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
         })
         .then(res => res.json()) // Extraire le corps de la réponse au format JSON
         .then(data => {
@@ -40,77 +48,78 @@ const Login = () => {
             console.log(data);
             // const token = {token: data.token};
             setTimeout(() => {
-                navigate('/login') ;
-            }, 1000); 
+                navigate('/login');
+            }, 1000);
             localStorage.setItem('token', data.token);
-            showNotif(data.success);
             // console.log(localStorage.getItem('token'));
         })
         .catch(err => {
             setError(err.msg);
-            showNotif(err.msg);
+            toast.error("Erreur lors de la connexion", {
+                autoClose: 1000
+            })
+        });*/
+
+        axios.post("http://localhost:1000/auth/login", data, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then((res) => {
+            setSuccess(res.data.success);
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('userId', res.data.userId);
+            // toast.success(success, {
+            //     autoClose: 1000
+            // });
+            toast.update(id, { render: success, type: "success", isLoading: false, autoClose: 3000 });
+            setTimeout(() => {
+                navigate(-1);
+            }, 1000);
+        }).catch(err => {
+            // toast.error(err.message, {
+            //     autoClose: 1000
+            // })
+            toast.update(id, { render: err.message, type: "error", isLoading: false, autoClose: 1000 });
         });
-        
+
         setIsLoading(false);
-            
+
     };
-    return ( 
+
+    return (
         <div className="login-form-container active">
 
-            {typeof authToken !== null ? (
-            !isLoading ? (
-                <form onSubmit={HandleSubmit}>
-                <h3>Connexion</h3>
-                <input type="email" name="" placeholder="Votre mail" className="box" />
-                <input type="password" name="" placeholder="Votre mot de passe" className="box" />
-                {/* <div className="remember">
-                    <input type="checkbox" name="" id="remember-me" />
-                    <label htmlFor="remember-me">Se souvenir de moi</label>
-                </div> */}
-                {error && <div className="alert alert-danger">{error}</div>}
-                {success && <div className="alert alert-success">{success}</div>}
-                <input type="submit" value="Se connecter" className="btn" />
-                <p>Mot de passe oublié ? <Link to="/">Cliquez ici</Link></p>
-                <p>Pas de compte ? <Link to="/register">En créer</Link></p>
+            {(!authToken && !isLoading) ? (
+                <form onSubmit={handleSubmit}>
+                    <h3>Connexion</h3>
+                    <input type="email" name="" ref={email} placeholder="Votre mail" className="box" />
+                    <input type="password" name="" ref={password} placeholder="Votre mot de passe" className="box" />
+                    {/* 
+                        <div className="remember">
+                            <input type="checkbox" name="" id="remember-me" />
+                            <label htmlFor="remember-me">Se souvenir de moi</label>
+                        </div> 
+                    */}
+                    <input type="submit" value="Se connecter" className="btn" />
+                    {/* <p>Mot de passe oublié ? <Link to="/">Cliquez ici</Link></p> */}
+                    <p>Pas de compte ? <Link to="/register">En créer</Link></p>
                 </form>
             ) : (
-                <div id="loader" className="loader-container">
-                <div className="loader"></div>
-                </div>
-            )
-            ) : (
-            <form>
-                <button className="btn" onClick={handleLogout}>
-                Déconnexion
-                </button>
-            </form>
+                <form>
+                    <button className="btn" onClick={handleLogout}>
+                        Déconnexion
+                    </button>
+                </form>
             )}
 
-
-            {/* { authToken != null ?
-                (
-                    !isLoading ?
-                        <form onSubmit={HandleSubmit} >
-                            <h3>Connexion</h3>
-                            <input type="email" name="" placeholder="Votre mail" className="box" />
-                            <input type="password" name="" placeholder="Votre mot de passe" className="box" />
-                            { error && <div className="alert alert-danger">{ error }</div> }
-                            { success && <div className="alert alert-success">{ success }</div> }
-                            <input type="submit" value="Se connceter" className="btn" />
-                            <p>Mot de passe oublié? <Link to="/">Cliquez ici</Link></p>
-                            <p>Pas de compte? <Link to="/register">En crée</Link></p>
-                        </form> :
-                    <div id="loader" class="loader-container">
-                        <div class="loader"></div>
-                    </div>
-                ) :
-                (<form>
-                    <button className='btn' onClick={handleLogout} >Déconnexion</button>
-                </form>)
-            } */}
+            {isLoading && (
+                <div id="loader" className="loader-container">
+                    <div className="loader"></div>
+                </div>
+            )}
 
         </div>
     );
 }
- 
+
 export default Login;
--- src/components/sections/register.jsx
+++ src/components/sections/register.jsx
@@ -1,21 +1,35 @@
-import { useContext, useRef, useState } from 'react';
+import axios from 'axios';
+import { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { NotifContext } from '../myHook/notif';
+import { toast } from 'react-toastify';
 // import UsePost from '../myHook/usePost';
 const Register = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [success, setSuccess] = useState(null);
-    const [error, setError] = useState(null);
+    const authToken = localStorage.getItem('token');
     const name = useRef();
     const mail = useRef();
     const number = useRef();
     const password = useRef();
     const navigate = useNavigate();
 
-    const { showNotif } = useContext(NotifContext);
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        toast.success("Vous etes déconnecté", {
+            autoClose: 1000
+        });
+        setTimeout(() => {
+            navigate(-1);
+        }, 1000);
+    };
 
-    const HandleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
+        const id = toast.loading("Traitement de l'iinscription en cours", {
+            theme: 'light',
+            pauseOnHover: false
+        });
         setIsLoading(true);
         const data = {
             username: name.current.value,
@@ -24,7 +38,7 @@ const Register = () => {
             password: password.current.value,
         };
 
-        fetch('http://localhost:1000/auth/register', 
+        /*fetch('http://localhost:1000/auth/register', 
         {
             method: "POST",
             headers: {
@@ -33,20 +47,43 @@ const Register = () => {
             body: JSON.stringify(data)
         })
         .then(res => res.json()) // Extraire le corps de la réponse au format JSON
-        .then(data => {
-            setSuccess(data.success);
-            console.log(data);
-            // const token = {token: data.token};
+        .then(response => {
+            setSuccess(response.data.success);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('userId', response.data.userId);
+            toast.success(success, {
+                autoClose: 1000
+            });
             setTimeout(() => {
-                navigate('/login') ;
-            }, 1000); 
-            // localStorage.setItem('user_data', JSON.stringify(user_data));
-            // showNotif(data.success);
-            // console.log(localStorage.getItem('token'));
+                navigate(-1) ;
+            }, 1000);
         })
         .catch(err => {
-            setError(err.msg);
-            showNotif(err.msg);
+            toast.error(err.message, {
+                autoClose: 1500
+            });
+        });*/
+
+        axios.post("http://localhost:1000/auth/register", data, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(res => {
+            setSuccess(res.data.success);
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('userId', res.data.userId);
+            // toast.success(success, {
+            //     autoClose: 1000
+            // });
+            toast.update(id, { render: success, type: "success", isLoading: false, autoClose: 1000 });
+            setTimeout(() => {
+                navigate(-1) ;
+            }, 1000);
+        }).catch(err => {
+            toast.update(id, { render: err.message, type: "error", isLoading: false, autoClose: 1000 });
+            // toast.error(err.message, {
+            //     autoClose: 1500
+            // });
         });
         
         setIsLoading(false);
@@ -56,24 +93,30 @@ const Register = () => {
         <div className="login-form-container active">
 
             {
-                !isLoading ?
-                <form onSubmit={HandleSubmit}>
+                (!authToken && !isLoading) ?
+                <form onSubmit={handleSubmit}>
                     <h3>Inscription</h3>
                     <input type="text" name="name" ref={name} placeholder="Votre nom" className="box" required />
                     <input type="email" name="email" ref={mail} placeholder="Votre mail" className="box" required />
                     <input type="tel" name="number" ref={number} placeholder="Votre numéro" className="box" required />
                     <input type="password" name="password" ref={password} placeholder="Votre mot de passe" className="box" required />
-                    { error && <div className="alert alert-danger">{ error }</div> }
-                    { success && <div className="alert alert-success">{ success }</div> }
                     <input type="submit" value="S'inscrire" className="btn" />
                     {/* <p>Mot de passe oublié? <Link to="/">Cliquez ici</Link></p> */}
                     <p>Déjà un compte? <Link to="/login">Se connceter</Link></p>
                 </form> :
+                <form>
+                    <button className="btn" onClick={handleLogout}>
+                        Déconnexion
+                    </button>
+                </form>
+            }
+            
+            {
+                isLoading &&
                 <div id="loader" class="loader-container">
                     <div class="loader"></div>
                 </div>
             }
-            
 
         </div>
     );
